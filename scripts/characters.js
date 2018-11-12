ec230f9b8f6b319ed6bfd85aa83bc4f8c8fe2d09
Thomassen <noveng3ance@gmail.com>
2018-11-12 15:11:28 +0100

    tried to add button to characters

--- scripts/characters.js
+++ scripts/characters.js
@@ -20,14 +20,38 @@ function Characters(result){
         var char = document.createElement('div');
         char.setAttribute('class', '[ char ]');
         char.setAttribute('id', result[i].aliases);
+        // Character info
         var char_title = document.createElement('h2');
         char_title.innerHTML = result[i].aliases;
         var char_gender = document.createElement('p');
         char_gender.innerHTML = result[i].gender;
+        // Add player tokens.
+        char_img = document.createElement('img');
+        char_img.src="https://via.placeholder.com/150";
+        // Add button.
+        char_button = document.createElement('input');
+        char_button.setAttribute('class', "selectBtn");
+        char_button.setAttribute('id', i);
+        char_button.type = "button";
+        char_button.value = "Play as " + result[i].aliases;
+        char_button.addEventListener("click", SelectHero());
         // Append everything
+        char.appendChild(char_img);
         char.appendChild(char_title);
         char.appendChild(char_gender);
+        char.appendChild(char_button);
         characters.appendChild(char);
     }
     container.appendChild(characters);
+};
+
+/*
+var buttons = document.getElementsByClassName('selectBtn');
+for(var i = 0; i < selectBtn.length; i++){
+    buttons.addEventListener('click', selectHero());
 }
+
+function selectHero(){
+    console.log("hello");
+}
+*/

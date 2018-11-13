87a170ae54648579968967f0f82741f0f6ddf7f2
Thomassen <JonasThomassen@outlook.com>
2018-11-13 14:48:48 +0100

    Selected hero gets a green border

--- scripts/characters.js
+++ scripts/characters.js
@@ -11,29 +11,30 @@ fetch('https://anapioficeandfire.com/api/characters')
 // Function to handle recieved data
 function Characters(result){
     console.log("Creating Characters...");
-    var container = document.getElementById("container");
-    var characters = document.createElement("div");
+    let container = document.getElementById("container");
+    let characters = document.createElement("div");
     characters.setAttribute('class', '[ characters ]');
     // Create the characters
-    for(var i = 0; i < result.length; i++){
+    for(let i = 0; i < result.length; i++){
         console.log(result[i]);
-        var char = document.createElement('div');
-        char.setAttribute('class', '[ char ]');
+        let char = document.createElement('div');
+        char.setAttribute('class', 'char');
         char.setAttribute('id', result[i].aliases);
         // Character info
-        var char_title = document.createElement('h2');
+        let char_title = document.createElement('h2');
         char_title.innerHTML = result[i].aliases;
-        var char_gender = document.createElement('p');
+        let char_gender = document.createElement('p');
         char_gender.innerHTML = result[i].gender;
         // Add player tokens.
         char_img = document.createElement('img');
         char_img.src="https://via.placeholder.com/150";
         // Add button.
         char_button = document.createElement('input');
-        char_button.setAttribute('class', "selectBtn");
+        char_button.setAttribute('class', "[ selectBtn ]");
         char_button.setAttribute('id', "button"+i);
         char_button.type = "button";
         char_button.value = "Play as " + result[i].aliases;
+        var buttonData = result[i].aliases;
         // Append everything
         char.appendChild(char_img);
         char.appendChild(char_title);
@@ -48,36 +49,58 @@ function Characters(result){
 // since button[i] became the last element in the list, the previous got overwritten. returning to this section later... 
 
 document.body.addEventListener( 'click', function(event){
-    if(event.srcElement.id == 'button0'){
-      console.log(event.srcElement.value);
-      // do shit
-    } else if(event.srcElement.id == 'button1'){
-        console.log(event.srcElement.value);
-        //do shit
-    } else if(event.srcElement.id == 'button2'){
-        console.log(event.srcElement.value);
-        //do shit
-    } else if(event.srcElement.id == 'button3'){
-        console.log(event.srcElement.value);
-        //do shit
-    } else if(event.srcElement.id == 'button4'){
-        console.log(event.srcElement.value);
-        //do shit
-    } else if(event.srcElement.id == 'button5'){
-        console.log(event.srcElement.value);
-        //do shit
-    } else if(event.srcElement.id == 'button6'){
-        console.log(event.srcElement.value);
-        //do shit
-    } else if(event.srcElement.id == 'button7'){
-        console.log(event.srcElement.value);
-        //do shit
-    } else if(event.srcElement.id == 'button8'){
-        console.log(event.srcElement.value);
-        //do shit
-    } else if(event.srcElement.id == 'button9'){
-        console.log(event.srcElement.value);
-        //do shit
+    let element = event.srcElement; 
+    if(element.id == 'button0'){
+        console.log(element.value);
+        clearButtons();
+        document.getElementById('The Daughter of the Dusk').style.border = "5px solid green";
+    } else if(element.id == 'button1'){
+        console.log(element.value);
+        clearButtons();
+        document.getElementById('Hodor').style.border = "5px solid green";
+    } else if(element.id == 'button2'){
+        console.log(element.value);
+        clearButtons();
+        document.getElementById('Lamprey').style.border = "5px solid green";
+    } else if(element.id == 'button3'){
+        console.log(element.value);
+        clearButtons();
+        document.getElementById('The Merling Queen').style.border = "5px solid green";
+    } else if(element.id == 'button4'){
+        console.log(element.value);
+        clearButtons();
+        document.getElementById('Old Crackbones').style.border = "5px solid green";
+    } else if(element.id == 'button5'){
+        console.log(element.value);
+        clearButtons();
+        document.getElementById('The Poetess').style.border = "5px solid green";
+    } else if(element.id == 'button6'){
+        console.log(element.value);
+        clearButtons();
+        document.getElementById('Porridge').style.border = "5px solid green";
+    } else if(element.id == 'button7'){
+        console.log(element.value);
+        clearButtons();
+        document.getElementById('Quickfinger').style.border = "5px solid green";
+    } else if(element.id == 'button8'){
+        console.log(element.value);
+        clearButtons();
+        document.getElementById("the Sailor's Wife").style.border = "5px solid green";
+    } else if(element.id == 'button9'){
+        console.log(element.value);
+        clearButtons();
+        document.getElementById('The Veiled Lady').style.border = "5px solid green";
     } 
 });
+
+
+
+// When a user clicks a button, remove the style of all other buttons
+function clearButtons(){
+    let el = document.querySelectorAll('.char');
+    console.log(el);
+    for(let i = 0; i < el.length; i++){
+        el[i].style.border = "none";
+    }
+}
 

bfd23812267d20ef157e4a9acf44853a42bd335e
Thomassen <noveng3ance@gmail.com>
2018-11-15 15:21:19 +0100

    Fixed button for both players

--- scripts/game.js
+++ scripts/game.js
@@ -1,7 +1,13 @@
 
 // Global Variables
 var canvas = document.getElementById("GameBoard");
+
+// Player Variables 
 var playerTurn = 1;
+var player1X = 45;
+var player1Y = 5;
+var player2X = 25;
+var player2Y = 50;
 
 // set up canvas
 if(canvas.getContext){
@@ -65,74 +71,45 @@ function buildTiles(amount, num, startX, startY, goX, goY, reverse){
 // Create game board using a function to create rows of tiles.
 function buildBoard(){
     ctx.beginPath();
+    buildTiles(10, 1, 0 , 0 ,true, false, false);
+    buildTiles(2, 11, 810, 90, false, true, false );
+    buildTiles(9, 13, 720, 180, false, false, true);
+    buildTiles(2, 14, 0, 270, false, true, false);
+    buildTiles(9, 16, 90, 360, true, false, false);
+    buildTiles(2, 24, 810, 450, false, true, false);
+    buildTiles(9, 26, 810, 540, false, false, true);
     ctx.fillStyle = "#FFFFFF";
-    ctx.fillRect(0, 0, 90, 90);
-    ctx.fillStyle = "#000000"
-    ctx.strokeRect(0, 0, 90, 90);
-    ctx.fillText("Start", 35, 45);
-    buildTiles(9, 2, 90, 0, true, false, false);
-    buildTiles(2, 9, 630, 90, false, true, false);
-    buildTiles(2, 11, 810, 90, false, true, false);
-    ctx.fillStyle = "#FFFFFF";
-    ctx.fillRect( 720, 180, 90, 90);
-    ctx.fillStyle = "#000000"
-    ctx.strokeRect( 720, 180, 90, 90);
-    ctx.fillText('11', 720 + 45, 180 + 45);
-    buildTiles(2, 13, 810, 270, false, true, false);
-    buildTiles(8, 15, 720, 360, false, false, true);
-    buildTiles(7, 11, 540, 180, false, false, true);
-    ctx.fillStyle = "#FFFFFF";
-    ctx.fillRect( 0, 270, 90, 90);
+    ctx.fillRect( 0, 540, 90, 90);
     ctx.fillStyle = "#000000";
-    ctx.strokeRect( 0, 270, 90, 90);
-    ctx.fillText('18', 0 + 45, 270 + 45);
-    ctx.fillStyle = "#FFFFFF";
-    ctx.fillRect(0, 360, 90, 90);
-    ctx.fillStyle = "#000000";
-    ctx.strokeRect(0, 360, 90,90);
-    ctx.fillText('23', 0 + 35, 360 + 45);
-    //  Longer board test
-    buildTiles(2, 24, 0, 450, false,true,false);
-    buildTiles(3, 26, 90, 540, true, false, false);
-    buildTiles(2, 15, 810, 450, false, true, false);
-    buildTiles(4, 17, 720, 540, false, false, true);
-    ctx.fillStyle = "#FFFFFF";
-    ctx.fillRect( 360, 540, 90, 90);
-    ctx.fillStyle = "#000000";
-    ctx.strokeRect( 360, 540, 90, 90);
-    ctx.fillText('Win', 360 + 35, 540 + 45);
+    ctx.strokeRect( 0, 540, 90, 90);
+    ctx.fillText('Kings Landing', 10, 540 + 45);
     ctx.stroke();
 }
 
 // Create player 1 token
 function playerOne(){
-    console.log('Placing Player 1...');
     var playerOne = localStorage.getItem('Player 1');
-    console.log('Player 1 is playing as: ' + playerOne);
     ctx.beginPath();
-    ctx.fillRect(45, 5, 35, 35);
+    ctx.fillRect(player1X, player1Y, 35, 35);
+    ctx.stroke();
 }
 
 // Create player 2 token
 function playerTwo(){
-    console.log('Placing Player 2...');
     var playerTwo = localStorage.getItem('Player 2');
-    console.log('Player 2 is playing as: ' + playerTwo);
     ctx.beginPath();
-    ctx.fillRect(25, 50, 35, 35);
+    ctx.fillRect(player2X, player2Y, 35, 35);
 }
 
 // Build game
 function init(){
-    console.log('Initializing game...');
     buildBoard();
     playerOne();
     playerTwo();
-    ctx.fillStyle = "#FFFFFF";
-    console.log('Game ready.');
+    ctx.stroke();
 }
 
-window.onload = init();
+window.onload = setInterval(init, 500);
 
 // on button click, check if its player 1 or 2's turn.
 function dice(){
@@ -143,7 +120,6 @@ function dice(){
         playerTurn = playerTurn + 1;
     } else if(playerTurn == 2){
         DiceResult(playerTurn);
-        console.log("Player 1's Turn");
         playerTurn = playerTurn -1;
         return playerTurn;
     }
@@ -152,11 +128,6 @@ function dice(){
 // Player X rolls the dice
 function DiceResult(playerTurn){
     // Gets a random number between 1 - 6
-    var Score = Math.round( Math.random() * 6 );
+    var Score = Math.round( Math.random() * 5 ) + 1;
     console.log('Player ' + playerTurn + " rolled a " + Score);
-    MovePlayer(playerTurn, Score);
-}
-
-function MovePlayer(playerTurn, Score){
-    console.log('Move player' + playerTurn + " " + Score + " Places.");
 }
